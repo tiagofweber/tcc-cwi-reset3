18c5cd4f7ee49167633670bd2d1debe116d85c07
Tiago Weber <webertiagof@gmail.com>
2021-10-31 14:50:33 -0300

    Adiciona regra que não permite cadastrar mais de um usuário por e-mail

---
+++ src/main/java/io/github/cwireset/tcc/exception/EmailDuplicadoException.java
@@ -0,0 +1,11 @@
+package io.github.cwireset.tcc.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmailDuplicadoException extends RuntimeException {
+    public EmailDuplicadoException(String email) {
+        super(String.format("Já existe um recurso do tipo Usuario com E-Mail com o valor '%s'.", email));
+    }
+}
--- src/main/java/io/github/cwireset/tcc/service/UsuarioService.java
+++ src/main/java/io/github/cwireset/tcc/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package io.github.cwireset.tcc.service;
 
 import io.github.cwireset.tcc.domain.Usuario;
+import io.github.cwireset.tcc.exception.EmailDuplicadoException;
 import io.github.cwireset.tcc.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public Usuario criarUsuario(Usuario usuario) {
+        boolean emailExists = usuarioRepository.existsByEmail(usuario.getEmail());
+
+        if (emailExists)
+            throw new EmailDuplicadoException(usuario.getEmail());
+
         return usuarioRepository.save(usuario);
     }
 }
